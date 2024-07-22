cc032e48a7fb4dbb86df3762069fad7dbee9e41c
Filipe Almeida <filipeagt.92@gmail.com>
2024-07-22 18:48:34 -0300

    Aula d013

--- desafios/d013/index.php
+++ desafios/d013/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <?php 
-        $v = $_POST['valor']??0;
+        $v = isset($_POST['valor'])?$_POST['valor']:0;
         $n100 = floor($v/100);
         $n50 = floor($v%100/50);
         $n10 = floor($v%100%50/10);
@@ -18,9 +18,9 @@
     <main>
         <h1>Caixa Eletrônico</h1>
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
-            <label for="valor">Qual valor você deseja sacar? (R$)</label>
-            <input type="number" name="valor" id="valor" step="5" value="<?=$v?>">
-            <p>*Notas disponíveis: R$100, R$50, R$10 e R$5</p>
+            <label for="valor">Qual valor você deseja sacar? (R$)<sup>*</sup></label>
+            <input type="number" name="valor" id="valor" step="5" value="<?=$v?>" required>
+            <p style="font-size: 0.7em;"><sup>*</sup>Notas disponíveis: R$100, R$50, R$10 e R$5</p>
             <input type="submit" value="Sacar">
         </form>
     </main> 
@@ -28,10 +28,10 @@
     <section>
         <h2>Saque de <?=numfmt_format_currency($padrao,$v,'BRL')?> realizado</h2>
         <p>O caixa eletrônico vai te entregar as seguintes notas:</p><ul>
-        <?="<li><img src='imagens/100-reais.jpg' alt='R$100'>x$n100</li>"?>
-        <?="<li><img src='imagens/50-reais.jpg' alt='R$50'>x$n50</li>"?>
-        <?="<li><img src='imagens/10-reais.jpg' alt='R$10'>x$n10</li>"?>
-        <?="<li><img src='imagens/5-reais.jpg' alt='R$5'>x$n5</li></ul>"?>
+        <?="<li><img class='nota' src='imagens/100-reais.jpg' alt='R$100'>x$n100</li>"?>
+        <?="<li><img class='nota' src='imagens/50-reais.jpg' alt='R$50'>x$n50</li>"?>
+        <?="<li><img class='nota' src='imagens/10-reais.jpg' alt='R$10'>x$n10</li>"?>
+        <?="<li><img class='nota' src='imagens/5-reais.jpg' alt='R$5'>x$n5</li></ul>"?>
     </section>
 </body>
 </html>
